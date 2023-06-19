dd77a7b2dda90f58fd068734d2feb19674c87b92
moondda <93575538+moondda@users.noreply.github.com>
2023-06-19 23:22:50 +0900

    [unrated] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,18 +1,13 @@
 function solution(a, b, n) { // 5 3 30
-    let new_cola=n; //30
     let count=0;
-    
-    //let nowGetCola =0;
-    while(new_cola>=a){ 
-    // count+=parseInt(new_cola/a); 
-    let cola=(parseInt(new_cola/a))*b //최대로 마시고 얻는 콜라 7
+    return newN(a,b,n,count);
+}
+
+function newN(a,b,n,count) {
+    if(n<a) return count;
+    let cola=(parseInt(n/a))*b //최대로 마시고 얻는 콜라 7
     count += cola
-    let remainder;
-    if(new_cola % a != 0 ) {
-        remainder= new_cola % a; //마시지 못하고 남은 콜라 2
-        new_cola= cola+remainder; 
-        }
-    else new_cola= cola;
-    }
-    return count;
+    let remainder = n % a; //마시지 못하고 남은 콜라 2
+    n= cola+remainder;
+    return newN(a,b,n,count);
 }
