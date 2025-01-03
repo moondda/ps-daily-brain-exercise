15945b010e8d5358560d68d81ba59eb91351a483
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-01-03 20:31:41 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.54 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,12 +1,9 @@
 function solution(numbers) {
-    const answer = [];
-    for(let i=0 ; i < numbers.length-1 ; i++) {
-        for(let j=i+1 ; j < numbers.length; j++) {
-            const num1 = numbers[i];
-            const num2 = numbers[j];
-            if(! answer.includes(num1+num2) ) answer.push(num1+num2);
+    const arr = [];
+    for(let i = 0 ; i < numbers.length-1; i++) {
+        for(let j=i+1; j<numbers.length; j++ ) {
+            arr.push(numbers[i]+numbers[j]);
         }
     }
-    
-    return answer.sort((a,b)=> a-b);
+    return [...new Set(arr)].sort((a,b)=> a-b);
 }
