3fe50b4d46fdb38b820f9158f44bbcd2459e40ff
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-01-04 15:50:32 +0900

    [level 1] Title: 모의고사, Time: 38.35 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,22 +1,26 @@
 function solution(answers) {
-    const answer1 = [1,2,3,4,5];
-    const answer2 = [2,1,2,3,2,4,2,5];
-    const answer3 = [3,3,1,1,2,2,4,4,5,5];
+    //예외! 가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요.
+    //answers를 순회하면서 각 패턴 index별로 내부 순회. 
+    // 만약 answers와 pattern이랑 같다면(나머지 연산자 이용) score에 1추가
     
-    let count1 = 0 ;
-    let count2 = 0 ;
-    let count3 = 0 ;
+    const patterns =[[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]];
+    const score =[0,0,0];
     
-    for(let i = 0 ; i < answers.length; i++) {
-        if(answer1[i % 5] === answers[i]) count1++;
-        if(answer2[i % 8] === answers[i]) count2++;
-        if(answer3[i % 10] === answers[i]) count3++;
+    for (const [i,answer] of answers.entries()) {
+        for (const [j,pattern] of patterns.entries()) {
+            if(answer === pattern[i%pattern.length]) score[j] += 1;
+        }
     }
-    const max = Math.max(count1,count2,count3);
-    const winner = [];
-        if(count1 === max) winner.push(1);
-        if(count2 === max) winner.push(2);
-       if(count3 === max) winner.push(3);
     
-    return winner.sort((a,b)=>a-b);
+    const max = Math.max(...score);
+    const winnerArr = [];
+    
+    for(const [i,eachScore] of score.entries()) {
+        if(eachScore===max) winnerArr.push(i+1);
+    }
+    
+    return winnerArr;
+    
+    
+    
 }
