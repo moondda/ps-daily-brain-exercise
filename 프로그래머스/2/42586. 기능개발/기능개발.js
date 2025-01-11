8e0e04af35288a856b61977bf67c0a900909a42c
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-01-11 14:57:16 +0900

    [level 2] Title: 기능개발, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,63 +1,19 @@
 function solution(progresses, speeds) {
-    const time = progresses.map((item,i)=> Math.ceil((100-item)/speeds[i]));
-    const queue = new Queue();
-    time.forEach(item => queue.push(item));
-    const result = [];
-    while(!queue.isEmpty()) {
-        let count = 1;
-        const comp = queue.pop();
-        if(queue.isEmpty()) {result.push(count); break;}
-        let flag = true;
-        while(flag && !queue.isEmpty() ) {
-            if(queue.head.data <= comp) {count++; queue.pop();}
-            else {flag = false; break;}
+    const dayCost = progresses.map((item,index)=> Math.ceil((100-item)/speeds[index]));
+    let deployDay = dayCost[0];
+    let count = 0;
+    const answerCounts = [];
+    
+    for(let i=0; i<progresses.length; i++) {
+        if(dayCost[i] <= deployDay) {
+            count++;
         }
-        result.push(count);
-    }
-    
-    return result;
-        
-    
-} 
-
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
-class Queue {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
-    }
-    
-    push(data) {
-        const newNode = new Node(data);
-        if(!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-        this.tail.next = newNode;
-        this.tail = newNode;
+        else {
+            answerCounts.push(count);
+            deployDay = dayCost[i];
+            count = 1;
         }
-        this.size++; 
     }
-    
-    pop() {
-        if(!this.head) return null;
-        const removedNode = this.head;
-        this.head = this.head.next;
-        if(!this.head) this.tail = null;
-        
-        this.size--;
-        return removedNode.data;;
-        
-    }
-    
-    isEmpty() {
-        return this.size === 0;
-    }
+    answerCounts.push(count);
+    return answerCounts;
 }
