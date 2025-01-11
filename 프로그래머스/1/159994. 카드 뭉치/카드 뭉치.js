bc5943180aa947f1d04a712fba9a7bf288d79067
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-01-11 15:28:09 +0900

    [level 1] Title: 카드 뭉치, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,16 +1,44 @@
 function solution(cards1, cards2, goal) {
-    let cards1Pointer = 0;
-    let cards2Pointer = 0;
+    let queue1 = new Queue(cards1);
+    let queue2 = new Queue(cards2);
+    let queueGoal = new Queue(goal);
     
-    for(let i=0 ; i<goal.length; i++) {
-        if(cards1[cards1Pointer] === goal[i] ) {
-            cards1Pointer++;
-        }
-        else if(cards2[cards2Pointer] === goal[i] ) {
-            cards2Pointer++;
-        }
-        else return "No";       
+    for(let i=0; i<goal.length; i++) {
+        if(!queue1.isEmpty() && queue1.first() === goal[i]) { queue1.pop(); queueGoal.pop(); }
+        else if(!queue2.isEmpty() && queue2.first() === goal[i]) { queue2.pop(); queueGoal.pop(); }
+        else break;
     }
     
-    return "Yes";
+    return queueGoal.isEmpty() ? "Yes" : "No";
 }
+
+class Queue{
+    items = [];
+    front = 0;
+    rear = 0;
+    
+    constructor(array){
+        this.items = array;
+        this.rear = array.length;
+    }
+    
+    push(item){
+        this.items.push(item);
+        this.rear++;
+    }
+    
+    pop(){
+        const removedNode = this.items[this.front];
+        this.front++;
+        return removedNode;
+    }
+    
+    first(){
+        return this.items[this.front];
+    }
+    
+    isEmpty(){
+        return this.front === this.rear;
+    }
+}
+
