8162779a4d1a8109284d2bb129c6f6d8ab7473c4
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-01-26 20:08:40 +0900

    [level 2] Title: 할인 행사, Time: 19.64 ms, Memory: 39.9 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,35 +1,26 @@
 function solution(want, number, discount) {
-    let answer = 0;
+    //10일동안 회원
+    //매일, 한가지 제품 할인, 하나씩만 구매할수있음 할인하는건
+    //10일 내내 제품을 원하는 개수만큼 살수있을때 회원가입
+    //할인 받을 수 있는 회원등록 날짜의 총 일수를 return 
+    //가능한 날이 없으면 0을 return
+    //number의 원소의 합은 10
+    
+    //시간복잡도 n^2이상이면 안됨
+    //알파벳 소문자로 이루어진 문자열
+    
+    var answer = 0;
     
     let wantParsed = {};
-    let currentProduct = {};
-    
-    let dayCount = 0;
     
     for(let i=0; i<want.length; i++) {
         wantParsed[want[i]] = number[i];
-        currentProduct[want[i]] = 0;
     }
     
+    console.log(wantParsed)
     
-    for(let i=0; i<discount.length; i++) {
-             if(discount[i] in wantParsed ) currentProduct[discount[i]] = (currentProduct[discount[i]] || 0) + 1;
-            dayCount++;
-            if(dayCount === 10 && isValid(wantParsed,currentProduct) ) {answer++;}
-            if(dayCount > 10)  {
-                dayCount--;
-                 if(discount[i-10] in wantParsed ) currentProduct[discount[i-10]] = Math.max(0, currentProduct[discount[i-10]] - 1);
-                if(isValid(wantParsed,currentProduct) ) answer++;
-            }
-    }
+//     for(let i=0; i<discount.length; i++) {
+//         console.log(wantParsed[discount[i]]);
+//     }
     return answer;
-}
-
-function isValid(wantParsed,currentProduct) {
-    for (let key in wantParsed) {
-                    if (wantParsed[key] > currentProduct[key]) {
-                        return false;
-                }
-}
-        return true;
 }
