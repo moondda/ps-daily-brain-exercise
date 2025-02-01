addefaeaacc4d316be3734e7fa87d4a32d2891bc
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-02-01 18:16:56 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 105.92 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,11 +1,20 @@
 function solution(participant, completion) {
-
-    participant.sort();
-    completion.sort();
+    const pplList = {};
     
-    for(let i=0; i<completion.length;i++) {
-        if(participant[i] !== completion[i]) return participant[i];
+    for(let i=0; i< participant.length; i++) {
+        //동명이인이 이미 있으면
+        if(participant[i] in pplList) pplList[participant[i]] += 1;
+        else pplList[participant[i]] = 1;
     }
-    return participant.at(-1);
     
+    for(let i=0; i<completion.length; i++) {
+        pplList[completion[i]] -= 1;
+    }
+    
+    const pplListArr =  Object.entries(pplList);
+    
+    for(let i=0; i<pplListArr.length; i++) {
+        if(pplListArr[i][1] === 1) return pplListArr[i][0];
+    }
+
 }
