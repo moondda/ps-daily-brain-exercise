c8166c1a69c48a68f7ba3b19e700113a1d6a678b
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-03-21 14:28:50 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 58.07 ms, Memory: 39.7 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -4,40 +4,50 @@ function solution(friends, gifts) {
     //선물 지수 = 이번 달까지 자신이 친구들에게 준 선물의 수 - 받은 선물의 수
     //만약 두 사람의 선물 지수도 같다면 다음 달에 선물을 주고받지 않습니다.
     //선물을 가장 많이 받을 친구가 받을 선물의 수 return
-    var answer = 0;
-    
-    const friendsIdx = {};
-    for(let i=0; i<friends.length; i++) {
-        friendsIdx[friends[i]] = i;
-    }
     const count = Array(friends.length).fill(0);
     
-    const giftsInfo = Array.from({length:friends.length}, ()=> Array(friends.length).fill(0));
+    const giftsInfo = {};
+    
+    for(let i=0; i<friends.length; i++) {
+        giftsInfo[friends[i]] = {};
+        for(let j=0; j<friends.length; j++) {
+            giftsInfo[friends[i]][friends[j]] = 0;
+        }
+    }
 
     for(let i of gifts) {
         const [from, to] = i.split(" ");
-        giftsInfo[friendsIdx[from]][friendsIdx[to]] += 1;
-    }
+        giftsInfo[from][to] += 1;
+        }
     
-    for(let i=0 ; i <giftsInfo.length; i++ ) {
-        for(let j=i+1; j <giftsInfo[0].length; j++) {
-            if ( giftsInfo[i][j] > giftsInfo[j][i] ) count[i] += 1;
-            else if ( giftsInfo[i][j] < giftsInfo[j][i] ) count[j] += 1;
-            else if ( giftsInfo[i][j] === giftsInfo[j][i] ) {
-                let rateI = giftsInfo[i].reduce((acc,cur)=>acc+= cur,0);
-                let tempI = 0;
-                for(let k=0; k<giftsInfo.length; k++) {
-                    tempI += giftsInfo[k][i];
+    
+    for(let i=0 ; i <friends.length-1; i++ ) {
+        for(let j=i+1; j <friends.length; j++) {
+            const from = friends[i];
+            const to = friends[j];
+            
+            if ( giftsInfo[from][to] > giftsInfo[to][from] ) count[i] += 1;
+            else if ( giftsInfo[from][to] < giftsInfo[to][from] ) count[j] += 1;
+            else if ( giftsInfo[from][to] === giftsInfo[to][from] ) {
+                
+                let rateI = 0 ,tempI =0;
+                for(let item in giftsInfo[from]) {
+                    rateI += giftsInfo[from][item];
+                }
+                for(let k of friends) {
+                    tempI += giftsInfo[k][from];
                 }
                 rateI -= tempI;
                 
-                let rateJ = giftsInfo[j].reduce((acc,cur)=>acc+= cur,0);
-                let tempJ = 0;
-                for(let k=0; k<giftsInfo.length; k++) {
-                    tempJ += giftsInfo[k][j];
+                let rateJ = 0;
+                for(let k in giftsInfo[to]) {
+                    rateJ += giftsInfo[to][k];
                 }
+                let tempJ = 0;
+                 for(let k of friends) {
+                    tempJ += giftsInfo[k][to];
+                } 
                 rateJ -= tempJ;
-                
                 if(rateI > rateJ) count[i] += 1;
                 else if (rateI < rateJ) count[j] += 1;
             }
