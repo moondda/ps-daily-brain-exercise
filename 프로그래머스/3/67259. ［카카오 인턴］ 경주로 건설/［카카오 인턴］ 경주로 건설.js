a80b8aa9fee2895e9174b3681f2a6838d1d37194
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-04-10 15:16:20 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 35.41 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
@@ -1,53 +1,52 @@
 function solution(board) {
-    //직선도로 100원
-    //코너 500원
-    //1 벽
-    //항상 도달 가능
     
-    const dx = [-1,0,1,0];
-    const dy = [0,-1,0,1];
-    const isVisited = Array.from({length: board.length}, ()=> Array(board.length).fill(0));
-    isVisited[0][0] = 1;
-    const queue = [{x:0, y:0,sum: 0, cmd : 'start', isVisited : isVisited}];
+    const dx=[-1,1,0,0];
+    const dy=[0,0,-1,1];
     
+    //[x][y][방향] 0위 1아래 2왼 3오
+    const costArr = Array.from({length: board.length} , () => Array.from({length : board[0].length}, () => Array(4).fill(Infinity)));
+    costArr[0][0][0] = 0;
+    costArr[0][0][1] = 0;
+    costArr[0][0][2] = 0;
+    costArr[0][0][3] = 0;
     
-   while(queue.length !== 0) {
-        const target = queue.shift();
-        if(target.x===board.length-1 && target.y===board[0].length-1){
-            return target.sum;
-        } 
+    const queue = [];
+    board[1][0] === 0 &&
+        queue.push({targetX:1,targetY:0,targetDir:1,targetCost:100});
+    board[0][1] === 0 &&
+        queue.push({targetX:0,targetY:1,targetDir:3,targetCost:100});
+    
+    while(queue.length > 0) {
+        const {targetX, targetY, targetDir, targetCost} = queue.pop();
         
         for(let i=0; i<4; i++) {
-            if(target.x + dx[i] < 0 || 
-                target.x + dx[i] >= board.length ||
-                target.y + dy[i] < 0 || 
-                target.y + dy[i] >= board[0].length ||
-               board[target.x + dx[i]][target.y + dy[i]] === 1 ||
-               target.isVisited[target.x + dx[i]][target.y + dy[i]] === 1
-                ) continue;
-                   
-            if(i === 0 || i === 2) {
-                const newVisit = target.isVisited.map(row => [...row]);
-                newVisit[target.x + dx[i]][target.y + dy[i]] = 1; 
-                
-                if(target.cmd === 'UD' || target.cmd === 'start' )
-                queue.push({x:target.x + dx[i], y:target.y + dy[i] , sum :target.sum + 100 , cmd : 'UD',isVisited : newVisit})
-                
-                if(target.cmd === 'LR')
-                queue.unshift({x:target.x + dx[i], y:target.y + dy[i] , sum :target.sum + 600 , cmd : 'UD',isVisited : newVisit})
+            if(targetX+dx[i] < 0 || targetX+dx[i] > board.length-1) continue;
+            if(targetY+dy[i] < 0 || targetY+dy[i] > board[0].length-1) continue;
+            if(board[targetX+dx[i]][targetY+dy[i]] == 1) continue;
+            
+            let addedCost = 0 ;
+            
+            if(targetDir == 0 || targetDir == 1) {
+                //100원 위아래
+                if(i === 0 || i === 1) addedCost = 100;
+                else if(i === 2 || i === 3) addedCost = 600;
             }
             
-            if(i === 1 || i === 3) {
-                const newVisit = target.isVisited.map(row => [...row]);
-                newVisit[target.x + dx[i]][target.y + dy[i]] = 1; 
-                
-                if(target.cmd === 'LR' || target.cmd === 'start')
-                queue.push({x:target.x + dx[i], y:target.y + dy[i] , sum :target.sum + 100 , cmd : 'LR',isVisited : newVisit})
-                if(target.cmd === 'UD')
-                queue.unshift({x:target.x + dx[i], y:target.y + dy[i] , sum :target.sum + 600 , cmd : 'LR',isVisited : newVisit})
-                 }  
+            else if(targetDir == 2 || targetDir == 3) {
+                //500원 위아래
+                if(i === 0 || i === 1) addedCost = 600;
+                else if(i === 2 || i === 3) addedCost = 100;
+            }
+            
+            if(costArr[targetX+dx[i]][targetY+dy[i]][i] >targetCost+addedCost) {
+                costArr[targetX+dx[i]][targetY+dy[i]][i] = targetCost+addedCost;
+                queue.push({targetX:targetX+dx[i], targetY:targetY+dy[i], targetDir: i, targetCost: targetCost+addedCost })
+            }
+            
             
         }
     }
+
+    return Math.min(...costArr[board.length-1][board[0].length -1]);
     
 }
