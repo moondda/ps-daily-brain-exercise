ee252766147d504cc24180c40527f7dd189b020a
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-05-08 18:19:31 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 24.37 ms, Memory: 42.7 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -1,36 +1,33 @@
 function solution(gems) {
-    //모든 종료 최소 1개 포함하는 가장 짧은 구간
-    //[시작번호, 끝번호] return
-    //여러개라면 시작번호가 가장 작은 구간 return
-    var answer = [];
-    const allGems = new Set();
-    for(let i of gems) {
-        allGems.add(i);
+    const allGems = new Set(); //모든 보석 종류
+    for(let gem of gems) {
+        allGems.add(gem);
     }
-    
-    const currentGems = new Map();
+    let currGems = new Map();
     let left = 0;
     let minLength = Infinity;
     let minArr = [];
     
     //i는 right
-    for(let i=0; i<gems.length;i++) {
-        if(currentGems.has(gems[i])) currentGems.set(gems[i], currentGems.get(gems[i]) + 1);
-        else currentGems.set(gems[i],1);
+    for(let i=0; i<gems.length; i++) {
+        currGems.set(gems[i], (currGems.get(gems[i]) || 0)+1);
         
-        while(currentGems.size === allGems.size) {
-            if(minLength > i-left+1) {
+        while(currGems.size === allGems.size) {
+            if(minLength > i-left + 1) {
                 minLength = i-left+1;
-                minArr= [left+1,i+1];
+                minArr = [left+1,i+1];
             }
             else if (minLength === i-left+1) {
                 if(minArr[0] > left) minArr = [left+1,i+1];
             }
             
-            if(currentGems.get(gems[left]) === 1) currentGems.delete(gems[left]);
-            else currentGems.set(gems[left], currentGems.get(gems[left])-1);
-            left += 1;
+            if(currGems.get(gems[left]) === 1) currGems.delete(gems[left]);
+                else currGems.set(gems[left], currGems.get(gems[left])-1);
+            left+=1;
         }
     }
+    
     return minArr;
+    
+    
 }
