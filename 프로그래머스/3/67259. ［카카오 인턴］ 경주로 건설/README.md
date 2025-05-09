f0c7e74a6a98d921908a49788345842d98dfa0b1
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-05-09 22:05:18 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 21.86 ms, Memory: 39.9 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 경주로 건설 - 67259 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259?language=javascript) 
 
 ### 성능 요약
 
-메모리: 38.5 MB, 시간: 35.41 ms
+메모리: 39.9 MB, 시간: 21.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 10일 15:16:17
+2025년 05월 09일 22:05:15
 
 ### 문제 설명
 
--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
@@ -1,52 +1,76 @@
 function solution(board) {
+    //1은 벽
+    var answer = 0;
     
-    const dx=[-1,1,0,0];
-    const dy=[0,0,-1,1];
+    //x,y,cost,dir 아래 위 왼 오
+    const queue = [[0,0,0,0]];
+    let minArr = Array.from({length:board.length}, ()=> Array.from({length:board[0].length} , () => Array(4).fill(Infinity)));
+    minArr[0][0][0] = 0;
+    minArr[0][0][1] = 0;
+    minArr[0][0][2] = 0;
+    minArr[0][0][3] = 0;
     
-    //[x][y][방향] 0위 1아래 2왼 3오
-    const costArr = Array.from({length: board.length} , () => Array.from({length : board[0].length}, () => Array(4).fill(Infinity)));
-    costArr[0][0][0] = 0;
-    costArr[0][0][1] = 0;
-    costArr[0][0][2] = 0;
-    costArr[0][0][3] = 0;
+    if (board[1][0] === 0) {
+        queue.push([1, 0, 100, 0]);
+        minArr[1][0][2] = 100;
+    }
+    if (board[0][1] === 0) {
+        queue.push([0, 1, 100, 3]);
+        minArr[0][1][1] = 100;
+    }
+
     
-    const queue = [];
-    board[1][0] === 0 &&
-        queue.push({targetX:1,targetY:0,targetDir:1,targetCost:100});
-    board[0][1] === 0 &&
-        queue.push({targetX:0,targetY:1,targetDir:3,targetCost:100});
-    
-    while(queue.length > 0) {
-        const {targetX, targetY, targetDir, targetCost} = queue.pop();
+    while(queue.length) { //나중에 포인터로 바꿔 아래 위 왼 오
+        const dx = [-1,1,0,0];
+        const dy = [0,0,-1,1];
+        const [x,y,cost,dir] = queue.shift();
+        
         
         for(let i=0; i<4; i++) {
-            if(targetX+dx[i] < 0 || targetX+dx[i] > board.length-1) continue;
-            if(targetY+dy[i] < 0 || targetY+dy[i] > board[0].length-1) continue;
-            if(board[targetX+dx[i]][targetY+dy[i]] == 1) continue;
+            const nextX = x+dx[i];
+            const nextY = y+dy[i];
             
-            let addedCost = 0 ;
+            if(nextX < 0 || nextX > board.length-1) continue;
+            if(nextY < 0 || nextY > board[0].length-1) continue;
+            if(board[nextX][nextY] === 1) continue;
+      
             
-            if(targetDir == 0 || targetDir == 1) {
-                //100원 위아래
-                if(i === 0 || i === 1) addedCost = 100;
-                else if(i === 2 || i === 3) addedCost = 600;
+           //     console.log(x,y,cost,dir,i)
+            if(dir === 0 || dir === 1) {
+                if(i === 0 || i === 1) {
+                    //비용이 최소면 100 추가
+                    if( minArr[nextX][nextY][i] > cost + 100) {
+                        minArr[nextX][nextY][i] = cost + 100;
+                        queue.push([nextX,nextY,minArr[nextX][nextY][i],i]);
+                    } 
+                }
+                if(i === 2 || i === 3) {
+                    //비용이 최소면 600 추가
+                    if( minArr[nextX][nextY][i] > cost + 600) {
+                        minArr[nextX][nextY][i] = cost + 600;
+                        queue.push([nextX,nextY,minArr[nextX][nextY][i],i]);
+                    } 
+                }
             }
-            
-            else if(targetDir == 2 || targetDir == 3) {
-                //500원 위아래
-                if(i === 0 || i === 1) addedCost = 600;
-                else if(i === 2 || i === 3) addedCost = 100;
+            else if(dir === 2 || dir === 3) {
+                if(i === 2 || i === 3) {
+                    //비용이 최소면 100 추가
+                    if( minArr[nextX][nextY][i] > cost + 100) {
+                        minArr[nextX][nextY][i] = cost + 100;
+                        queue.push([nextX,nextY,minArr[nextX][nextY][i],i]);
+                    } 
+                }
+                if(i === 0 || i === 1) {
+                    //비용이 최소면 600 추가
+                    if( minArr[nextX][nextY][i] > cost + 600) {
+                        minArr[nextX][nextY][i] = cost + 600;
+                        queue.push([nextX,nextY,minArr[nextX][nextY][i],i]);
+                    } 
+                }
             }
-            
-            if(costArr[targetX+dx[i]][targetY+dy[i]][i] >targetCost+addedCost) {
-                costArr[targetX+dx[i]][targetY+dy[i]][i] = targetCost+addedCost;
-                queue.push({targetX:targetX+dx[i], targetY:targetY+dy[i], targetDir: i, targetCost: targetCost+addedCost })
-            }
-            
-            
+                       
         }
     }
 
-    return Math.min(...costArr[board.length-1][board[0].length -1]);
-    
+    return Math.min(...minArr[board.length-1][board[0].length-1]);
 }
