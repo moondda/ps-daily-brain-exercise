2ae8dfd42bde834d33e5f779480b59999122b450
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-05-09 17:35:36 +0900

    [level 3] Title: 징검다리 건너기, Time: 28.18 ms, Memory: 43.9 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
@@ -1,32 +1,24 @@
 function solution(stones, k) {
-    //밝으면 -1
-    //0이면 더이상 밟을수없음
-    //건너뛸수있음. 가장 가까운 디딤돌로만
     
-    let right = 200000000;
     let left = 1;
+    let right = 200000000;
     
-    const canCross = (mid,k) => {
-        let skip = 0;
-        for(let i=0; i<stones.length; i++) {
-            if(stones[i] < mid) skip++;
-            else skip = 0;
-            
-            if(skip >= k) return false;
+    const canCross = (mid) => {
+        let count = 0;
+        for(let i of stones) {
+            if(i<=mid) count++;
+            else count =0;
+            if(count === k) return false;
         }
         return true;
     }
     
     while(left <= right) {
-        const mid = Math.floor((left+right)/2);
-        if(canCross(mid,k)) {
-            left = mid+1;
-        }
-        else right = mid-1;
-        
-        
+        const mid = Math.floor((left + right) /2);
+        if(canCross(mid)) left = mid+1;
+        else right = mid-1;     
     }
     
-    return left-1;
+    return left;
 }
     
