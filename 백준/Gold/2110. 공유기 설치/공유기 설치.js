73db2dd77245af4b62376eb5272b7edd4acc7410
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-05-09 23:37:03 +0900

    [Gold IV] Title: 공유기 설치, Time: 244 ms, Memory: 36516 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.js
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.js
@@ -8,22 +8,21 @@ const [N, C] = input.shift().split(" ").map(Number);
 input = input.map(Number).sort((a, b) => a - b);
 
 let left = 1;
-let right = input[N - 1] - input[0];
+let right = input[input.length - 1] - input[0];
 
 while (left <= right) {
-  let mid = Math.floor((left + right) / 2);
-  let lastInstalled = input[0];
-  let count = 1;
+  const mid = Math.floor((left + right) / 2); //인접한 두 공유기 최대 거리
+  let lastInstalled = input[0]; //1개 일단 설치
+  let installCount = 1;
   for (let i = 1; i < input.length; i++) {
-    if (input[i] - lastInstalled >= mid) {
-      count += 1;
+    if (mid <= input[i] - lastInstalled) {
+      installCount += 1;
       lastInstalled = input[i];
     }
   }
-
-  if (count >= C) {
+  if (installCount >= C) {
     left = mid + 1;
   } else right = mid - 1;
 }
 
-console.log(left - 1);
+console.log(right);
