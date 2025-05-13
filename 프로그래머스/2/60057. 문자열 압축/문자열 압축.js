67aaf334dacbd84fbe938bf0cfefd4da5fd5f552
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-05-13 18:07:55 +0900

    [level 2] Title: 문자열 압축, Time: 0.92 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
@@ -1,45 +1,30 @@
 function solution(s) {
-    //문자열에서 같은 값이 연속해서 나타나는 것을 그 문자의 개수와 반복되는 값으로 표현
-    //"aabbaccc"의 경우 "2a2ba3c" , 1은 생략
-    //문자열은 제일 앞부터 정해진 길이만큼 잘라야 합니다.
-    var answer = 0;
+    let length = 1;
+    let minLength = s.length;
     
-    const maxUnit= Math.floor(s.length);
-    let minResult = s.length; //갱신해나감
-    
-    for(let i=1; i< maxUnit; i++) {
-        const sArr = [];
-        let sTemp = '';
-        for(let j=0; j<s.length; j+= i) {
-            sTemp = s.slice(j,j+i);
-            sArr.push(sTemp);
-        }
-      // console.log(sArr,"arr")
-        
-        const finalArr = [];
-        let pattern = sArr[0];
+    while(length <= s.length/2) {  
+        let recent = '';
         let count = 1;
-        for(let k=1; k<sArr.length; k++){
-            if(sArr[k] === pattern) count++;
+        let arr = ''; //이번 턴에 만들어지는 배열
+        for(let i=0; i<s.length; i+=length) { 
+            const curr = s.slice(i,i+length);
+            if(recent === curr) count++;
+            
+            //다르면 1. 전꺼 푸시 2. 이번거 초기화
             else {
-                if(count > 1) {finalArr.push(String(count)+ pattern);}
-                else {
-                    finalArr.push(pattern);
-                 //   finalArr.push(sArr[k]);
-                }
+                if(count === 1) arr += recent;
+                else arr += `${count}${recent}`;
                 
-                //갱신
-                pattern = sArr[k];
+                recent = curr;
                 count = 1;
             }
-        }
-        
-        if(count > 1) {finalArr.push(String(count)+ pattern);}
-        else finalArr.push(pattern);
-
-        if(finalArr.join("").length < minResult) 
-            minResult = finalArr.join("").length;
-        
     }
-    return minResult;
+        if(count === 1) arr += recent;
+        else arr += `${count}${recent}`;
+        
+        if(arr.length < minLength) minLength = arr.length;
+        length++;  
+    }
+    
+    return minLength;
 }
