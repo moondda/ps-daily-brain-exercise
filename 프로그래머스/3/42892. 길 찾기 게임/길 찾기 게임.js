c9d6847b9ae695775d05bea2217740fc9e1e394a
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-05-17 18:36:36 +0900

    [level 3] Title: 길 찾기 게임, Time: 31.63 ms, Memory: 46.2 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.js
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.js
@@ -1,54 +1,47 @@
+class TreeNode {
+    constructor(node) {
+        this.x = node[0];
+        this.y = node[1];
+        this.idx = node[2];
+        this.left = null;
+        this.right = null;
+    }
+    
+    insert(node) {
+        //왼쪽 노드
+        if(node[0] < this.x) {
+            if(!this.left) this.left = new TreeNode(node);
+            else this.left.insert(node);
+        }
+        //오른쪽 노드
+        else {
+            if(!this.right) this.right = new TreeNode(node);
+            else this.right.insert(node);
+        }
+    }
+}
+
 function solution(nodeinfo) {
-    const nodes = nodeinfo.map((e,index) => ({x:e[0], y:e[1] , index : index+1})).sort((a,b)=> {
-        if(b.y > a.y) return 1;
-        else if(b.y === a.y) return a.x - b.x;
+    var answer = [[]];
+    nodeinfo = nodeinfo.map((e,idx)=> [...e,idx+1]);
+    //[x,y,idx]
+    nodeinfo.sort((a,b)=>{
+        if(b[1]>a[1]) return 1;
+        else if(a[1]==b[1]) return a[0]-b[0];
         else return -1
     });
-    
-    
-    class TreeNode {
-        constructor(node) {
-            this.x = node.x;
-            this.y = node.y;
-            this.id = node.index;
-            this.left = null;
-            this.right = null;
-        }
-        
-        insert(node) {
-            if(node.x < this.x) {
-                if(!this.left) {
-                    this.left = new TreeNode(node);
-                }
-                else this.left.insert(node);
-            } else {
-                if(!this.right) {
-                    this.right = new TreeNode(node);
-                }
-                else this.right.insert(node);
-            }
-        }
+    const tree = new TreeNode(nodeinfo[0]);
+    for(let i=1; i<nodeinfo.length; i++) tree.insert(nodeinfo[i]);
+    const front = [];
+    const back = [];
+    const dfs = (root) => {
+        if(!root) return;
+        front.push(root.idx);
+        dfs(root.left);
+        dfs(root.right);
+        back.push(root.idx);
     }
     
-    const root = new TreeNode(nodes[0]);
-    
-    for(let i=1 ; i<nodes.length;i++) root.insert(nodes[i]);
-    
-    const preorder = [];
-    const postorder = [];
-    
-    const traversal = (node) => {
-        if(!node) return;
-        preorder.push(node.id);
-        traversal(node.left);
-        traversal(node.right);
-        postorder.push(node.id);
-        
-    }
-    
-    traversal(root);
-    
-    return [preorder,postorder]
-    
-    
+    dfs(tree);
+    return [front,back];
 }
