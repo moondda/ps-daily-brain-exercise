add8726604f4e21601885faa28aa857334c0f074
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-05-31 16:32:59 +0900

    [level 3] Title: 기둥과 보 설치, Time: 114.65 ms, Memory: 41.7 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/README.md
+++ 프로그래머스/3/60061. 기둥과 보 설치/README.md
@@ -1,10 +1,10 @@
 # [level 3] 기둥과 보 설치 - 60061 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60061?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60061) 
 
 ### 성능 요약
 
-메모리: 40 MB, 시간: 69.29 ms
+메모리: 41.7 MB, 시간: 114.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 07일 20:42:32
+2025년 05월 31일 16:32:56
 
 ### 문제 설명
 
--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.js
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.js
@@ -1,94 +1,86 @@
 function solution(n, build_frame) {
-    //build frame [가로,세로,기둥이면0 보면1, 삭제면0 설치면1]
-    //return [x,y,a] x,y는 보와 기둥의 교차점. a는 기둥이면0 보면 1
-    //보의 조건
-    //기둥의 조건
+    //[x,y,a,b] 0은 기둥 1은 보, 0은 삭제, 1은 설치
+    //보 오른쪽 방향으로 설치, 기둥 위쪽 방향으로 설치
     
-    const curr0 = new Set(); //기둥
-    const curr1 = new Set(); //보
+    const giSet = new Set();
+    const boSet = new Set();
     
-    const isValid0 = () => {
-        for(let i of curr0){ //기둥
-            const [x,y] = i.split(",").map(Number);
-            //바닥
-            if(y===0) continue;
-            //보 한쪽 끝 위
-            else if(curr1.has(`${x-1},${y}`) || curr1.has(`${x},${y}`)) continue;
-            //다른 기둥 위
-            else if(curr0.has(`${x},${y-1}`)) continue;
-            return false;  
-        }
-        return true;
+    //기둥 규칙
+    //바닥 위에 있거다,
+    //보의 한쪽 끝 부분 위에 있거나
+    //다른 기둥 위에 위치해야함
+    const installGi = (x,y) => {
+        if(y==0 ||
+          boSet.has(`${x},${y}`) ||
+          boSet.has(`${x-1},${y}`) ||
+          giSet.has(`${x},${y-1}`)) return true;
+        else return false;
     }
     
-    const isValid1 = () => {
-        for(let i of curr1){ //보
-            const [x,y] = i.split(",").map(Number);
-            //한쪽 끝 부분이 기둥 위
-            if(curr0.has(`${x},${y-1}`) || curr0.has(`${x+1},${y-1}`)  ) continue;
-            //양쪽 끝 부분이 다른 보와 동시에 연결
-            else if(curr1.has(`${x-1},${y}`) && curr1.has(`${x+1},${y}`)) continue;
-            return false;  
-        }
-        return true;
-    }
-    
-    for(let build of build_frame) {
-        const [x,y,a,b] = build;
         
-        //기둥이면
-        if(a===0) {
-            //삭제일때
-            if(b===0) {
-                curr0.delete(`${x},${y}`);
-                if(!isValid0() || !isValid1()) curr0.add(`${x},${y}`);  
-            }
-            //설치일때    
-            if(b===1) {
-                //바닥
-                if(y===0) curr0.add(`${x},${y}`);
-                //보 한쪽 끝 위
-                else if(curr1.has(`${x-1},${y}`) || curr1.has(`${x},${y}`))
-                    curr0.add(`${x},${y}`);
-                //다른 기둥 위
-                else if(curr0.has(`${x},${y-1}`))
-                    curr0.add(`${x},${y}`);
-            }
-        }
-        
-        //보 일때
-        else if(a===1) {
-            //삭제일때
-            if(b===0) {
-                curr1.delete(`${x},${y}`);
-                if(!isValid0() || !isValid1()) curr1.add(`${x},${y}`);  
-            }
-                
-            //설치일때    
-            if(b===1) {
-                //한쪽 끝 부분이 기둥 위
-                if(curr0.has(`${x},${y-1}`) || curr0.has(`${x+1},${y-1}`)  )
-                    curr1.add(`${x},${y}`);
-                //양쪽 끝 부분이 다른 보와 동시에 연결
-                else if(curr1.has(`${x-1},${y}`) && curr1.has(`${x+1},${y}`))
-                    curr1.add(`${x},${y}`);
-            }
-        }
+    //보 규칙
+    //한쪽 끝이 기둥 위에 잇거나,
+    //양쪽 끝 부분이 다른 보와 동시에 연결
+    const installBo = (x,y) => {
+        if(giSet.has(`${x+1},${y-1}`) || giSet.has(`${x},${y-1}`))
+            return true;  
+        else if(boSet.has(`${x-1},${y}`) && boSet.has(`${x+1},${y}`)) return true;
+        else return false;
     }
     
-    let result = [];
-    for(let curr of curr0) {
-        const [x,y] = curr.split(",").map(Number);
-        result.push([x,y,0]);     
+    const available = () => {
+        for(let gi of giSet) {
+            const [x,y] = gi.split(",").map(Number);
+            if(!installGi(x,y)) return false;
+        }
+        for(let bo of boSet) {
+            const [x,y] = bo.split(",").map(Number);
+            if(!installBo(x,y)) return false;
+        }      
+        return true;      
     }
-    for(let curr of curr1) {
-        const [x,y] = curr.split(",").map(Number);
-        result.push([x,y,1]);     
+    
+    const deleteGi = (x,y) => {
+        giSet.delete(`${x},${y}`);
+        if(available()) return true;
+        else return false;
     }
-
-    return result.sort((a,b)=> {
-        if(a[0] != b[0]) return a[0]-b[0];
-        else if(a[1] !== b[1]) return a[1]-b[1];
-        else return a[2]-b[2];
+    
+    const deleteBo = (x,y) => {
+        boSet.delete(`${x},${y}`);
+        if(available()) return true;
+        else return false;
+    }
+    
+    for(let [x,y,a,b] of build_frame) {
+        //기둥 설치
+        if(a==0 && b==1) {
+            if(installGi(x,y)) giSet.add(`${x},${y}`);
+        }
+        //보 설치
+        if(a==1 && b==1) {
+           if(installBo(x,y)) boSet.add(`${x},${y}`);
+           }
+        //기둥 삭제
+        if(a==0 && b==0) {
+            if(!deleteGi(x,y)) giSet.add(`${x},${y}`);
+        } 
+        //보 삭제
+        if(a==1 && b==0) {
+            if(!deleteBo(x,y)) boSet.add(`${x},${y}`);
+        }
+        
+    }
+    //0은 기둥 1은 보
+    const giTemp = [...giSet].map((e)=> [...e.split(",").map(Number),0]);
+    const boTemp = [...boSet].map((e)=> [...e.split(",").map(Number),1]);
+    
+    //리턴 [x,y,a] a의 0은 기둥 1은 보
+    //x 오름차순, y오름차순, 보 먼저
+    const temp = [...giTemp,...boTemp].sort((a,b)=> {
+        if(a[0] !== b[0]) return a[0]-b[0];
+        if(a[1] !== b[1]) return a[1]-b[1];
+        return a[2] - b[2];       
     });
+    return temp;
 }
