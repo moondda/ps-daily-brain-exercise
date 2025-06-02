34d2be194e3aee92052eb164d63fc05774d1a852
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-06-02 13:43:05 +0900

    [Gold III] Title: 줄 세우기, Time: 336 ms, Memory: 54300 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.js
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.js
@@ -5,30 +5,32 @@ let input = require("fs")
   .split("\n");
 
 input = input.map((e) => e.split(" ").map(Number));
-//N: 학생수 , M: 키 비교 횟수
+
 const [N, M] = input.shift();
 
-const indegree = Array(N + 1).fill(0);
 const graph = Array.from({ length: N + 1 }, () => []);
+const indegree = Array(N + 1).fill(0);
 
 for (let [a, b] of input) {
-  indegree[b] += 1;
   graph[a].push(b);
+  indegree[b] += 1;
 }
 
 const queue = [];
-for (let i = 1; i <= N; i++) {
+
+for (let i = 1; i < indegree.length; i++) {
   if (indegree[i] === 0) queue.push(i);
 }
-
 const result = [];
 while (queue.length > 0) {
   const target = queue.shift();
+
   result.push(target);
 
-  for (const next of graph[target]) {
-    indegree[next]--;
-    if (indegree[next] === 0) queue.push(next);
+  for (let i of graph[target]) {
+    indegree[i] -= 1;
+
+    if (indegree[i] === 0) queue.push(i);
   }
 }
 
