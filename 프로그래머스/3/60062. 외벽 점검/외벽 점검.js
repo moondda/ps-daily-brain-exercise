55003d7448ea54d3fa05e2a51969e112c7684ac6
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-07-04 17:07:05 +0900

    [level 3] Title: 외벽 점검, Time: 1478.67 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.js
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.js
@@ -1,18 +1,42 @@
 function solution(n, weak, dist) {
+    //dist조합 1명부터 dist.length 명까지 다 조합해보고 있으면 최소 명수 리턴, 없으면 -1리턴.
+     //[1,5,6,10,13,17,18,22] [2,4]
+    const weakAdd = weak.map((e)=> e+n);
+    const allWeak = [...weak,...weakAdd];
     
-        const getPermutations = (arr) => {
-        if (arr.length === 0) return [[]];
+    const slicedWeak = [];
+    
+    for(let i=0; i<weak.length; i++) {
+        slicedWeak.push(allWeak.slice(i,i+weak.length));
+    }
+    
+    const permutation = (arr,k) => {
+        if(k===1) return arr.map(e=> [e]);
+        
         const result = [];
-        for (let i = 0; i < arr.length; i++) {
-            const rest = [...arr.slice(0, i), ...arr.slice(i + 1)];
-            console.log(rest,"rest")
-            const restPerms = getPermutations(rest);
-            for (const perm of restPerms) {
-                result.push([arr[i], ...perm]);
+        
+        arr.forEach((fixed,idx,origin) => {
+            const rest = [...origin.slice(0,idx),...origin.slice(idx+1)];
+            const perms = permutation(rest,k-1);
+            const attached = perms.map((e)=> [fixed,...e]);
+            result.push(...attached);
+        });
+        
+        return result;
+    }
+    
+    for(let i=1; i<=dist.length; i++) {
+        const perms = permutation(dist,i); //i명이 담긴 조합후보 [[1,2],[2,3],[1,3]]
+        for(let sliced of slicedWeak) {
+            for(let perm of perms) { //[1,2]
+                let weakCandi = [...sliced];
+                for(let p of perm) {
+            const covered = weakCandi[0] + p;
+            weakCandi = weakCandi.filter((e)=> e>covered);
+            if(weakCandi.length === 0) return i;
+                }
             }
         }
-        return result;
-            
-    };
-            const k = getPermutations([1,2]);
+    }
+    return -1;
 }
