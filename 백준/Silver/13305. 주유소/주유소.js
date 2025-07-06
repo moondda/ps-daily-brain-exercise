6b4ee8eab80f1d0b0498115fcc6b4e2821b4c86b
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-07-06 21:48:20 +0900

    [Silver III] Title: 주유소, Time: 176 ms, Memory: 19704 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -4,48 +4,18 @@ let input = require("fs")
   .trim()
   .split("\n");
 
-const N = input.shift();
+const N = Number(input.shift());
+const dists = input.shift().split(" ").map(Number);
+const prices = input.shift().split(" ").map(Number);
 
-input = input.map((e) => e.split(" ").map(Number));
-const dists = input.shift();
-const prices = input.shift();
+let totalCost = 0;
+let minPrice = prices[0];
 
-let currOil = 0;
-let currCost = 0;
-let currIdx = 0;
-while (currIdx < prices.length - 1) {
-  //도착했으면 break;
-  let sumDist = 0;
-  let cangoIdx = currIdx;
-
-  for (let j = currIdx + 1; j < prices.length; j++) {
-    if (prices[currIdx] > prices[j]) {
-      cangoIdx = j;
-      for (let k = currIdx; k < j; k++) {
-        sumDist += dists[k];
-      }
-    }
-    //앞으로 최대는 없음
-    else {
-      cangoIdx = prices.length - 1;
-      sumDist = 0;
-      for (let k = currIdx; k < dists.length; k++) {
-        sumDist += dists[k];
-      }
-    }
-
-    let needOil = sumDist - currOil;
-    if (needOil > 0) {
-      currOil += needOil;
-      currCost += prices[currIdx] * currOil;
-    }
-
-    //앞으로 나가기
-    //오일 소비
-    currOil -= sumDist;
-    //거리 갱신
-    currIdx = cangoIdx;
+for (let i = 0; i < N - 1; i++) {
+  if (prices[i] < minPrice) {
+    minPrice = prices[i];
   }
+  totalCost += minPrice * dists[i];
 }
 
-console.log(currCost);
+console.log(totalCost);
