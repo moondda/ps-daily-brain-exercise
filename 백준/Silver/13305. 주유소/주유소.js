5edf7bfe5b4dd895f7d5dfcf883abcb8122a0006
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-07-12 18:12:32 +0900

    [Silver III] Title: 주유소, Time: 288 ms, Memory: 44904 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -1,21 +1,22 @@
 let input = require("fs")
   .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
   .toString()
-  .trim()
   .split("\n");
 
-const N = Number(input.shift());
-const dists = input.shift().split(" ").map(Number);
-const prices = input.shift().split(" ").map(Number);
+const cityCount = +input.shift();
 
-let totalCost = 0;
-let minPrice = prices[0];
+input = input.map((e) => e.split(" ").map(BigInt));
 
-for (let i = 0; i < N - 1; i++) {
-  if (prices[i] < minPrice) {
-    minPrice = prices[i];
-  }
-  totalCost += minPrice * dists[i];
+//거점에 도착했을때
+//다음 거점까지의 거리까지 진행
+let minPark = Infinity;
+let sum = 0n;
+
+for (let i = 0; i < input[1].length - 1; i++) {
+  const park = input[1][i];
+  if (park < minPark) minPark = park;
+  const dist = input[0][i];
+  sum += dist * minPark;
 }
 
-console.log(totalCost);
+console.log(String(sum));
