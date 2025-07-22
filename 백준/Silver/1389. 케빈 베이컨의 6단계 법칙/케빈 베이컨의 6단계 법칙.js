629e138515480f043e2926c0f4eb33dce9772b1f
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-07-22 20:30:07 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 168 ms, Memory: 11792 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.js
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.js
@@ -8,30 +8,18 @@ let input = require("fs")
 let [N, M] = input.shift(); //n명의 유저수
 
 const graph = Array.from({ length: N + 1 }, () => []);
-
-for (let [a, b] of input) {
-  graph[a].push(b);
-  graph[b].push(a);
-}
-
 let dist = Array.from({ length: N + 1 }, () => Array(N + 1).fill(Infinity));
 
+for (let [a, b] of input) {
+  dist[a][b] = 1;
+  dist[b][a] = 1;
+}
+
 for (let i = 1; i <= N; i++) {
-  const queue = [[i, 0]];
-  let isVisited = Array(N + 1).fill(false);
-  isVisited[i] = true;
-
-  while (queue.length > 0) {
-    const [curr, count] = queue.shift();
-    if (dist[i][curr] > count) {
-      dist[i][curr] = count;
-      dist[curr][i] = count;
-    }
-
-    for (let c of graph[curr]) {
-      if (!isVisited[c]) {
-        isVisited[c] = true;
-        queue.push([c, count + 1]);
+  for (let j = 1; j <= N; j++) {
+    for (let k = 1; k <= N; k++) {
+      if (dist[j][k] > dist[j][i] + dist[i][k]) {
+        dist[j][k] = dist[j][i] + dist[i][k];
       }
     }
   }
