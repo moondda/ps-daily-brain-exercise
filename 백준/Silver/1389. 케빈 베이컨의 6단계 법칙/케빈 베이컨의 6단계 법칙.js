4c6d38bcde6dd17a1edeb88d81b807e30c29562b
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-07-22 20:13:40 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 172 ms, Memory: 12316 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.js
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.js
@@ -14,50 +14,24 @@ for (let [a, b] of input) {
   graph[b].push(a);
 }
 
-let answerArr = Array(N + 1).fill(0);
-const countMap = new Map();
+let dist = Array.from({ length: N + 1 }, () => Array(N + 1).fill(Infinity));
 
-for (let j = 1; j <= N; j++) {
-  for (let i = 1; i <= N; i++) {
-    //같은 인물
-    if (i == j) continue;
+for (let i = 1; i <= N; i++) {
+  const queue = [[i, 0]];
+  let isVisited = Array(N + 1).fill(false);
+  isVisited[i] = true;
 
-    //이미 전에 계산함
-    if (j > i) {
-      if (countMap.get(`${i}, ${j}`)) {
-        answerArr[j] += countMap.get(`${i}, ${j}`);
-        continue;
-      }
-    } else if (j < i) {
-      if (countMap.get(`${j}, ${i}`)) {
-        answerArr[j] += countMap.get(`${j}, ${i}`);
-        continue;
-      }
+  while (queue.length > 0) {
+    const [curr, count] = queue.shift();
+    if (dist[i][curr] > count) {
+      dist[i][curr] = count;
+      dist[curr][i] = count;
     }
 
-    const queue = [[j, 0]];
-    let isVisited = Array(N + 1).fill(false);
-    isVisited[j] = true;
-
-    while (queue.length > 0) {
-      const [curr, count] = queue.shift();
-      if (curr === i) {
-        answerArr[j] += count;
-
-        if (j > i) {
-          countMap.set(`${i}, ${j}`, count);
-        } else if (j < i) {
-          countMap.set(`${j}, ${i}`, count);
-        }
-
-        break;
-      }
-
-      for (let c of graph[curr]) {
-        if (!isVisited[c]) {
-          isVisited[c] = true;
-          queue.push([c, count + 1]);
-        }
+    for (let c of graph[curr]) {
+      if (!isVisited[c]) {
+        isVisited[c] = true;
+        queue.push([c, count + 1]);
       }
     }
   }
@@ -65,8 +39,11 @@ for (let j = 1; j <= N; j++) {
 
 let minNum = Infinity;
 let minPpl = -1;
-for (let i = 1; i <= N; i++) {
-  const value = answerArr[i];
+for (let i = 1; i < dist.length; i++) {
+  let value = 0;
+  for (let j = 1; j < dist[i].length; j++) {
+    value += dist[i][j];
+  }
   if (value < minNum) {
     minNum = value;
     minPpl = i;
