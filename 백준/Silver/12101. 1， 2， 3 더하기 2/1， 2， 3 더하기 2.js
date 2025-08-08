0963cde8f85abdf37674ddd168453802293b48a1
Dahyun Moon <93575538+moondda@users.noreply.github.com>
2025-08-08 15:04:04 +0900

    [Silver I] Title: 1, 2, 3 더하기 2, Time: 100 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.js
+++ 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.js
@@ -4,18 +4,12 @@ let input = require("fs")
   .trim()
   .split("\n");
 
-const [N, k] = input.shift().split(" ").map(Number);
+let [N, k] = input.shift().split(" ").map(Number);
 
 let dp = Array(N + 1).fill(1);
 
 dp[1] = 1;
 dp[2] = 2;
-//1+2 2+1 3
-//dp[3] = dp[2] + dp[1] + dp[0];
-//1+3 2+2 3+1
-//dp[4] = dp[3] + dp[2] + dp[1];
-// //1+4 2+3 3+2
-// dp[5] = dp[4] + dp[3] + dp[2];
 
 for (let i = 3; i <= N; i++) {
   dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
@@ -23,18 +17,23 @@ for (let i = 3; i <= N; i++) {
 
 if (dp[N] < k) console.log("-1");
 else {
-  let counter = 0;
-  let currIndex = N;
+  let counter = N;
   const result = [];
-  while (counter < k) {
-    for (let i = 1; i <= 3; i++) {
-      counter += dp[currIndex - i];
-      if (counter >= k) {
-        counter -= dp[currIndex - i];
-        result.push(i);
-        currIndex = currIndex - i;
-        break;
-      }
+  while (counter > 0) {
+    const c1 = dp[counter - 1] ?? 0;
+    const c2 = dp[counter - 1] + (dp[counter - 2] ?? 0);
+
+    if (k <= c1) {
+      result.push(1);
+      counter -= 1;
+    } else if (k <= c2) {
+      result.push(2);
+      k -= c1;
+      counter -= 2;
+    } else {
+      k -= c2;
+      result.push(3);
+      counter -= 3;
     }
   }
 
